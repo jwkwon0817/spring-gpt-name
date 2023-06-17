58a44a08a4c7ce1eb16763b574db06ebace083c7
jwkwon0817 <jeewon.kwon.0817@gmail.com>
2023-06-17 18:31:22 +0900

    Spring GPT Name

--- src/main/java/com/codemos/name/domain/service/GptService.java
+++ src/main/java/com/codemos/name/domain/service/GptService.java
@@ -1,6 +1,7 @@
 package com.codemos.name.domain.service;
 
 import com.codemos.name.domain.dto.ResultDto;
+import com.codemos.name.secrets.Api;
 import com.theokanning.openai.completion.CompletionRequest;
 import com.theokanning.openai.service.OpenAiService;
 import lombok.extern.slf4j.Slf4j;
@@ -11,7 +12,7 @@ public class GptService {
 	public ResultDto result(String usage, String description, String caseStyle, String language) {
 		String prompt = "I'm trying to name a " + usage + " in " + language + " that " + description + ". The Code style is " + caseStyle + ". Please suggest a name for me.";
 		
-		OpenAiService service = new OpenAiService("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");
+		OpenAiService service = new OpenAiService(Api.OPEN_AI_API_KEY);
 		CompletionRequest completionRequest = CompletionRequest.builder()
 			.prompt(prompt)
 			.model("text-davinci-003")
---
+++ src/main/java/com/codemos/name/secrets/Api.java
@@ -0,0 +1,5 @@
+package com.codemos.name.secrets;
+
+public class Api {
+	public static final String OPEN_AI_API_KEY = "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";
+}
